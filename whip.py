e060f51572f64be62db9f436163c90cb5d4a760b
Nort4 <60520357+Nort4@users.noreply.github.com>
2020-06-02 15:24:53 +0300

    Create whip.py

---
+++ whip.py
@@ -0,0 +1,20 @@
+from telethon import events
+import os
+import requests
+import json
+from uniborg.util import admin_cmd
+from socket import *
+
+@borg.on(admin_cmd(pattern="ip (.*)"))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+
+    input_str = event.pattern_match.group(1)
+        
+    addr = gethostbyname(input_str)
+    await event.edit("IP Address of website is" + " " + addr)
