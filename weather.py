4ae39472cfeab0579e69591d2d119a817524401a
Nort4 <60520357+Nort4@users.noreply.github.com>
2020-06-02 19:56:09 +0300

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,28 @@
+from telethon import events
+import os
+import json
+from urllib import request
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="weat (.*)"))
+async def _(event):
+	
+    if event.fwd_from:
+        return
+
+
+    location = event.pattern_match.group(1)
+
+    wind = 'M'
+    metric = 'm'
+    option = '0T'
+
+	  url = (f'http://wttr.in/{location}?F{wind}{metric}{option}')
+
+
+
+	  rqst = request.urlopen(url)
+	  if rqst.getcode() in [200]:
+    	  charset = rqst.info().get_content_charset()
+    	  content = rqst.read().decode(charset)
+    	  await event.edit(content.split('Weather ')[2])
