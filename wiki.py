7032052498fa257d132ad887c3f0653210928892
Nort4 <60520357+Nort4@users.noreply.github.com>
2020-07-27 08:35:16 +0300

    Create wiki.py

---
+++ wiki.py
@@ -0,0 +1,18 @@
+from telethon import events
+import os
+from uniborg.util import admin_cmd
+import wikipedia
+
+wikipedia.set_lang("ru")
+
+
+await message.edit(sasat)
+@borg.on(admin_cmd(pattern="wiki (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+
+    input_str = event.pattern_match.group(1)
+    
+    wiki_search = wikipedia.summary(input_str)
+    await message.edit(wiki_search)
